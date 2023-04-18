8744484a04e5e42693a9a81dd7d1af5241cfe9d2
Atif Shafi <ashafi@redhat.com>
2023-04-18 11:18:55 -0400

    updated port number

--- app/app.py
+++ app/app.py
@@ -480,4 +480,4 @@ if __name__ == '__main__':
     create_database()
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5001)
